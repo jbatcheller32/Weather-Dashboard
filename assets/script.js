db38914ebd8f5a0fddcc0d6817e43c142cce8b7c
J Mac <jbatcheller123@gmail.com>
2023-11-23 10:17:41 -0700

    trying to understand how to make an array for the search history to display on page

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-//var searchEl = document.querySelector('#citySearch');
+var searchEl = document.querySelector('#citySearch');
 var citySearchBtnEl = document.querySelector('#searchBtn');
 
 
@@ -6,24 +6,20 @@ var citySearchBtnEl = document.querySelector('#searchBtn');
 // button event listener to store display the search history on the page
 
 citySearchBtnEl.addEventListener('click', function (event) {
-event.preventDefault();
+    event.preventDefault();
 
-    var userSearchEl = document.querySelector('#userSearch').value;
-    localStorage.setItem('search', userSearchEl);
+var userSearchEl = document.querySelector('#userSearch').value;
+localStorage.setItem('search', userSearchEl);
 
-    document.querySelector('#userSearch').value = '';
+document.querySelector('#userSearch').value = '';
 
-    const storedSearchValue = JSON.parse(localStorage.getItem('search')) || [];
+const storedSearchValue = localStorage.getItem('search');
 
-    var resutlEl = document.querySelector('#search-results');
-    resutlEl.textContent = storedSearchValue;
-   
+var resultEl = document.querySelector('#search-results');
+resultEl.textContent = storedSearchValue;
 
-   
 
-    resutlEl.setAttribute("style", "background-color: #5982a1; border: solid 1px; maring-top: 20px; width: 20%; list-style: none;");
+resultEl.setAttribute("style", "background-color: #5982a1; border: solid 1px; maring-top: 20px; width: 20%; list-style: none;");
 
 
 });
-
-

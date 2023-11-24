daee677b7ab47650c9933bcdbbf092f1c8e02305
J Mac <jbatcheller123@gmail.com>
2023-11-24 10:10:52 -0700

    thanks to the todos exercise I now understand how local storage works a little more. Finally have the search history rendering on the page!

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,72 @@
 var searchEl = document.querySelector('#citySearch');
+var userInputEl = document.querySelector('#userSearch');
+var listEl = document.querySelector('#search-results');
 var citySearchBtnEl = document.querySelector('#searchBtn');
+var recentSearches = [];
 
 
+// the function below creates the li's for the search history to be displayed on the page
+
+function renderHistory() {
+
+    userInputEl.innerHTML = "";
+
+
+    for (var i = 0; i < recentSearches.length; i++)
+        var searches = recentSearches[i];
+
+    var li = document.createElement("li");
+    li.textContent = searches;
+    li.setAttribute("index", i);
+
+    listEl.appendChild(li);
+
+}
+
+// this function retrieves the stored search history from local storage
+
+function getSearches() {
+
+    var storedSearches = JSON.parse(localStorage.getItem("searches"));
+
+    if (storedSearches !== null) {
+        recentSearches = storedSearches;
+    }
+
+    renderHistory();
+}
+
+
+// this function is setting the searches to local storage and setting them to the array of recent searches
+
+
+function storeSearches() {
+
+    localStorage.setItem("searches", JSON.stringify(recentSearches));
+}
 
 // button event listener to store display the search history on the page
 
 citySearchBtnEl.addEventListener('click', function (event) {
     event.preventDefault();
 
-var userSearchEl = document.querySelector('#userSearch').value;
-localStorage.setItem('search', userSearchEl);
+    var searchText = userInputEl.value.trim();
 
-document.querySelector('#userSearch').value = '';
+    if (searchText === "") {
+        return;
+    }
 
-const storedSearchValue = localStorage.getItem('search');
+    recentSearches.push(searchText);
+    userInputEl.value = "";
 
-var resultEl = document.querySelector('#search-results');
-resultEl.textContent = storedSearchValue;
+    storeSearches();
+    renderHistory();
 
-
-resultEl.setAttribute("style", "background-color: #5982a1; border: solid 1px; maring-top: 20px; width: 20%; list-style: none;");
 
 
 });
+
+getSearches();
+
+
+

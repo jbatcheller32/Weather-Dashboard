d4e183c155431cedcaa41dee7323de0f5ae2b520
J Mac <jbatcheller123@gmail.com>
2023-11-24 14:35:32 -0700

    added the card for current day weather, I have absolutely no idea how to let the user search any city and have it convert to lat lon

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,49 @@ var searchEl = document.querySelector('#citySearch');
 var userInputEl = document.querySelector('#userSearch');
 var listEl = document.querySelector('#search-results');
 var citySearchBtnEl = document.querySelector('#searchBtn');
+var currentDayWeatherEl = document.querySelector('#card');
+var currentDayListEl = document.querySelector('#card-text');
 var recentSearches = [];
+
+
+function getApi() {
+
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=39.7392&lon=-104.9847&appid=a7bb081342ad5ee187c71f2c065ec901';
+
+   
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+
+            var cardTitle = document.createElement('h5');
+            cardTitle.textContent = data.city.name + ' ' + dayjs().format('MM/DD/YYYY');
+
+            currentDayWeatherEl.appendChild(cardTitle);
+            cardTitle.setAttribute("style", "font-size: 30px; margin-top: -19px;");
+
+            for (var i = 0; i < data.list.length; i++) {
+
+                var temp = document.createElement('li');
+                
+
+                temp.textContent = 'Tempurature:' + " " + data.list[i].main.temp;
+                currentDayWeatherEl.appendChild(temp);
+                temp.setAttribute("style", "list-style-type: none; margin-left: 5px; "); 
+                return; 
+
+            }
+        });
+
+};
+
+
+citySearchBtnEl.addEventListener('click', getApi);
+
+
+
 
 
 // the function below creates the li's for the search history to be displayed on the page
@@ -20,6 +62,7 @@ function renderHistory() {
     li.setAttribute("index", i);
 
     listEl.appendChild(li);
+    li.setAttribute("style", " width: 20%; margin-top: 20px; background-color: #76848f; color: #fff; border-radius: 5px; text-align: center;");
 
 }
 
@@ -66,7 +109,7 @@ citySearchBtnEl.addEventListener('click', function (event) {
 
 });
 
-getSearches();
+//getSearches();
 
 
 

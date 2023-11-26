b0cfcb91efc6bbe49e1c161c5945f8dc7126e17a
J Mac <jbatcheller123@gmail.com>
2023-11-26 13:04:51 -0700

    I have added some css to the card, and also have all the card rendering todays weather. Still have no idea to make it so I can search any city with the API

--- assets/script.js
+++ assets/script.js
@@ -2,16 +2,26 @@ var searchEl = document.querySelector('#citySearch');
 var userInputEl = document.querySelector('#userSearch');
 var listEl = document.querySelector('#search-results');
 var citySearchBtnEl = document.querySelector('#searchBtn');
-var currentDayWeatherEl = document.querySelector('#card');
+var currentDayWeatherEl = document.querySelector('#main-card');
 var currentDayListEl = document.querySelector('#card-text');
+var dayTwoCardEl = document.querySelector('#card-1');
+var dayTwoListEl = document.querySelector('#card-text-1');
+var dayThreeCardEl = document.querySelector('#card-2');
+var dayThreeListEl = document.querySelector('#card-text-2');
+var dayFourCardEl = document.querySelector('#card-3');
+var dayFourListEl = document.querySelector('#card-text-3');
+var dayFiveCardEl = document.querySelector('#card-4');
+var dayFiveListEl = document.querySelector('#card-text-4');
+var daySixCardEl = document.querySelector('#card-5');
+var daySixListEl = document.querySelector('#card-text-5');
 var recentSearches = [];
 
 
 function getApi() {
 
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=39.7392&lon=-104.9847&appid=a7bb081342ad5ee187c71f2c065ec901';
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?&lat=39.7392&lon=-104.9847&appid=a7bb081342ad5ee187c71f2c065ec901&units=imperial';
 
-   
+
     fetch(requestUrl)
         .then(function (response) {
             return response.json();
@@ -19,23 +29,171 @@ function getApi() {
         .then(function (data) {
             console.log(data);
 
+            //the code belwo creates a title and the datr
+
             var cardTitle = document.createElement('h5');
             cardTitle.textContent = data.city.name + ' ' + dayjs().format('MM/DD/YYYY');
+
+            var cardTwoTitle = document.createElement('h5');
+            cardTwoTitle.textContent = data.city.name + ' ' + dayjs().format('MM/DD/YYYY');
+
+            var cardThreeTitle = document.createElement('h5');
+            cardThreeTitle.textContent = data.city.name + ' ' + dayjs().format('MM/DD/YYYY');
+
+            var cardFourTitle = document.createElement('h5');
+            cardFourTitle.textContent = data.city.name + ' ' + dayjs().format('MM/DD/YYYY');
+
+            var cardFiveTitle = document.createElement('h5');
+            cardFiveTitle.textContent = data.city.name + ' ' + dayjs().format('MM/DD/YYYY');
+
+            var cardSixTitle = document.createElement('h5');
+            cardSixTitle.textContent = data.city.name + ' ' + dayjs().format('MM/DD/YYYY');
+
 
             currentDayWeatherEl.appendChild(cardTitle);
             cardTitle.setAttribute("style", "font-size: 30px; margin-top: -19px;");
 
+            dayTwoCardEl.appendChild(cardTwoTitle);
+            cardTwoTitle.setAttribute("style", "font-size: 16px; margin-top: -19px;");
+
+            dayThreeCardEl.appendChild(cardThreeTitle);
+            cardThreeTitle.setAttribute("style", "font-size: 16px; margin-top: -19px;");
+
+            dayFourCardEl.appendChild(cardFourTitle);
+            cardFourTitle.setAttribute("style", "font-size: 16px; margin-top: -19px;");
+
+            dayFiveCardEl.appendChild(cardFiveTitle);
+            cardFiveTitle.setAttribute("style", "font-size: 16px; margin-top: -19px;");
+
+            daySixCardEl.appendChild(cardSixTitle);
+            cardSixTitle.setAttribute("style", "font-size: 16px; margin-top: -19px;");
+
+            //all of these for loops retrieve the temp, wind, and humidity and append them to a list for each card. This seems like wayyyy more code than I would need, but 
+            //I am not sure how to consolidate this, so this is what we get
+
             for (var i = 0; i < data.list.length; i++) {
 
                 var temp = document.createElement('li');
-                
+                var wind = document.createElement('li');
+                var humidity = document.createElement('li');
 
                 temp.textContent = 'Tempurature:' + " " + data.list[i].main.temp;
                 currentDayWeatherEl.appendChild(temp);
-                temp.setAttribute("style", "list-style-type: none; margin-left: 5px; "); 
-                return; 
+                temp.setAttribute("style", "list-style-type: none; margin-left: 5px; ");
 
+                wind.textContent = 'Wind:' + " " + data.list[i].wind.speed;
+                currentDayWeatherEl.appendChild(wind);
+                wind.setAttribute("style", "list-style-type: none; margin-left: 5px; ");
+
+                humidity.textContent = 'Humidity:' + " " + data.list[i].main.humidity;
+                currentDayWeatherEl.appendChild(humidity);
+                humidity.setAttribute("style", "list-style-type: none; margin-left: 5px; ");
+
+
+                for (var i = 0; i < data.list.length; i++) {
+
+                    var temp = document.createElement('li');
+                    var wind = document.createElement('li');
+                    var humidity = document.createElement('li');
+
+
+                    temp.textContent = 'Tempurature:' + " " + data.list[i].main.temp;
+                    dayTwoCardEl.appendChild(temp);
+                    temp.setAttribute("style", "list-style-type: none; margin-left: 5px; ");
+
+                    wind.textContent = 'Wind:' + " " + data.list[i].wind.speed;
+                    dayTwoCardEl.appendChild(wind);
+                    wind.setAttribute("style", "list-style-type: none; margin-left: 5px; ");
+
+                    humidity.textContent = 'Humidity:' + " " + data.list[i].main.humidity;
+                    dayTwoCardEl.appendChild(humidity);
+                    humidity.setAttribute("style", "list-style-type: none; margin-left: 5px; ");
+
+
+                    for (var i = 0; i < data.list.length; i++) {
+
+                        var temp = document.createElement('li');
+                        var wind = document.createElement('li');
+                        var humidity = document.createElement('li');
+
+                        temp.textContent = 'Tempurature:' + " " + data.list[i].main.temp;
+                        dayThreeCardEl.appendChild(temp);
+                        temp.setAttribute("style", "list-style-type: none; margin-left: 5px; ");
+
+                        wind.textContent = 'Wind:' + " " + data.list[i].wind.speed;
+                        dayThreeCardEl.appendChild(wind);
+                        wind.setAttribute("style", "list-style-type: none; margin-left: 5px; ");
+
+                        humidity.textContent = 'Humidity:' + " " + data.list[i].main.humidity;
+                        dayThreeCardEl.appendChild(humidity);
+                        humidity.setAttribute("style", "list-style-type: none; margin-left: 5px; ");
+
+                        for (var i = 0; i < data.list.length; i++) {
+
+                            var temp = document.createElement('li');
+                            var wind = document.createElement('li');
+                            var humidity = document.createElement('li');
+
+                            temp.textContent = 'Tempurature:' + " " + data.list[i].main.temp;
+                            dayFourCardEl.appendChild(temp);
+                            temp.setAttribute("style", "list-style-type: none; margin-left: 5px; ");
+
+                            wind.textContent = 'Wind:' + " " + data.list[i].wind.speed;
+                            dayFourCardEl.appendChild(wind);
+                            wind.setAttribute("style", "list-style-type: none; margin-left: 5px; ");
+
+                            humidity.textContent = 'Humidity:' + " " + data.list[i].main.humidity;
+                            dayFourCardEl.appendChild(humidity);
+                            humidity.setAttribute("style", "list-style-type: none; margin-left: 5px; ");
+
+                            for (var i = 0; i < data.list.length; i++) {
+
+                                var temp = document.createElement('li');
+                                var wind = document.createElement('li');
+                                var humidity = document.createElement('li');
+
+                                temp.textContent = 'Tempurature:' + " " + data.list[i].main.temp;
+                                dayFiveCardEl.appendChild(temp);
+                                temp.setAttribute("style", "list-style-type: none; margin-left: 5px; ");
+
+                                wind.textContent = 'Wind:' + " " + data.list[i].wind.speed;
+                                dayFiveCardEl.appendChild(wind);
+                                wind.setAttribute("style", "list-style-type: none; margin-left: 5px; ");
+
+                                humidity.textContent = 'Humidity:' + " " + data.list[i].main.humidity;
+                                dayFiveCardEl.appendChild(humidity);
+                                humidity.setAttribute("style", "list-style-type: none; margin-left: 5px; ");
+
+
+                                for (var i = 0; i < data.list.length; i++) {
+
+                                    var temp = document.createElement('li');
+                                    var wind = document.createElement('li');
+                                    var humidity = document.createElement('li');
+
+                                    temp.textContent = 'Tempurature:' + " " + data.list[i].main.temp;
+                                    daySixCardEl.appendChild(temp);
+                                    temp.setAttribute("style", "list-style-type: none; margin-left: 5px; ");
+
+                                    wind.textContent = 'Wind:' + " " + data.list[i].wind.speed;
+                                    daySixCardEl.appendChild(wind);
+                                    wind.setAttribute("style", "list-style-type: none; margin-left: 5px; ");
+
+                                    humidity.textContent = 'Humidity:' + " " + data.list[i].main.humidity;
+                                    daySixCardEl.appendChild(humidity);
+                                    humidity.setAttribute("style", "list-style-type: none; margin-left: 5px; ");
+                                    return;
+
+                                }
+
+                            }
+
+                        }
+
+                    }
+                }
             }
+
         });
 
 };
@@ -109,7 +267,7 @@ citySearchBtnEl.addEventListener('click', function (event) {
 
 });
 
-//getSearches();
+getSearches();
 
 
 
